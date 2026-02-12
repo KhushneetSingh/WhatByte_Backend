3888ac97be5b40f66680f5bfe2df709115d1957c
Khushneet Singh <singhkhushneet601@gmail.com>
2026-02-12 21:45:46 +0530

    adding the mapping and checking doctor in postman

--- core/views.py
+++ core/views.py
@@ -9,6 +9,8 @@ from .serializers import (
     DoctorSerializer,
     MappingSerializer
 )
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -31,7 +33,13 @@ class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
 
-
+    
 class MappingViewSet(viewsets.ModelViewSet):
     queryset = PatientDoctorMapping.objects.all()
     serializer_class = MappingSerializer
+
+    @action(detail=False, methods=['get'], url_path='patient/(?P<patient_id>[^/.]+)')
+    def get_doctors_for_patient(self, request, patient_id=None):
+        mappings = PatientDoctorMapping.objects.filter(patient_id=patient_id)
+        serializer = self.get_serializer(mappings, many=True)
+        return Response(serializer.data)
